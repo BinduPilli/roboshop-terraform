d68ea477d64d453c87bb8fb77dfb185dfb90836f
Bindu P <bindupilli1314@gmail.com>
2023-05-06 03:52:02 +0530

    Terraform

--- servers.tf
+++ servers.tf
@@ -9,27 +9,6 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "null_resource" "provisioner" {
-
-  depends_on = [aws_instance.instance, aws_route53_record.records]
-  for_each = var.components
-  provisioner "remote-exec" {
-
-    connection {
-      type = "ssh"
-      user = "centos"
-      password = "DevOps321"
-      host = aws_instance.instance[each.value["name"]].private_ip
-    }
-
-    inline = [
-      "rm -rf roboshop-shell",
-      "git clone https://github.com/BinduPilli/roboshop-shell",
-      "cd roboshop-shell"
-      "sudo bash ${each.value["name"]}.sh"
-    ]
-  }
-}
 
 resource "aws_route53_record" "records" {
   for_each = var.components
